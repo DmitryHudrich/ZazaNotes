8b377d7ab72ec4e01fc17d60d1413fc6c1b68ad3
DarkFos <darkfos82@gmail.com>
2024-03-24 00:35:28 +0300

    Что за приколы, питон?

--- ZazaBot/src/utils/text.py
+++ ZazaBot/src/utils/text.py
@@ -22,10 +22,6 @@ async def text_for_my_profile(data_my_profile: dict) -> str:
     :return:
     """
 
-    my_profile_text: str = f"""
-    <b><i>Мой профиль</i></b>\n\n
-    <b>Моё имя</b>: {data_my_profile.get('info')['firstName'] + ' ' + (data_my_profile.get('info')['lastName'] if data_my_profile.get('info')['lastName'] else '')}\n\n
-    <b>Описание профиля</b>: \n\n{data_my_profile.get('info')['description'] if data_my_profile.get('info')['description'] else 'Отсутствует'}\n\n
-    """
+    my_profile_text: str = f"""<b><i>Мой профиль</i></b>\n\n<b>Моё имя</b>: {data_my_profile.get('info')['firstName'] + ' ' + (data_my_profile.get('info')['lastName'] if data_my_profile.get('info')['lastName'] else '')}\n\n<b>Описание профиля</b>: \n\n{data_my_profile.get('info')['description'] if data_my_profile.get('info')['description'] else 'Отсутствует'}\n\n"""
     return my_profile_text
 
