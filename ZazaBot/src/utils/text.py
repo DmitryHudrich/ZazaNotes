0837a823535890a122b67c15c35024c9adcbae7d
DarkFos <darkfos82@gmail.com>
2024-03-24 00:06:22 +0300

    Что за приколы, питон?

--- ZazaBot/src/utils/text.py
+++ ZazaBot/src/utils/text.py
@@ -23,7 +23,7 @@ async def text_for_my_profile(data_my_profile: dict) -> str:
     """
 
     my_profile_text: str = (f"<b><i>Мой профиль</i></b>\n\n" +
-                            f"<b>Моё имя</b>: {data_my_profile.get('info')['firstName'] + " " + (data_my_profile.get("info")['lastName']
+                            f"<b>Моё имя</b>: {data_my_profile.get('info')['firstName'] + " " + (data_my_profile.get('info')['lastName']
                             if data_my_profile.get('info')['lastName'] else '')}\n\n" +
                             f"<b>Описание профиля</b>: \n\n{data_my_profile.get('info')['description'] if data_my_profile.get('info')
                             ['description'] else 'Отсутствует'}\n\n")
