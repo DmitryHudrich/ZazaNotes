db6321a3d0aeabc16aebe1a9a99021408a0d9b3a
w1thluv <dimahudric@gmail.com>
2024-03-25 21:26:55 +0300

    changed password quality rules logic

--- Zaza/Web/Stuff/InteractLogic/Auth/Auth.cs
+++ Zaza/Web/Stuff/InteractLogic/Auth/Auth.cs
@@ -10,20 +10,9 @@ internal sealed class AuthInteractions(ILogger<AuthInteractions> logger, Reposit
         var status = true;
 
         if (!State.DisablePasswordValidation) {
-            passwordQuality = AuthHelper.ValidatePassword(userDTO.Password);
+            passwordQuality = ValidatePassword(userDTO.Password);
         }
-
-        if (passwordQuality == PasswordQuality.BAD) {
-            var err = $"{userDTO} password is too weak";
-            status = false;
-            logger.LogDebug(err);
-        };
-
-        if (!status || !await UserRepository.AddAsync(userDTO)) {
-            var err = $"{userDTO} wasn't added to database";
-            status = false;
-            logger.LogDebug(err);
-        };
+        status = await CheckPasswordQuality(userDTO, passwordQuality, status);
 
         var res = passwordQuality switch {
             PasswordQuality.STRONG => new InteractResult<PasswordQuality>(status, InteractEvent.AUTHORIZATION, passwordQuality),
@@ -41,5 +30,45 @@ internal sealed class AuthInteractions(ILogger<AuthInteractions> logger, Reposit
             var err = $"{userDTO.Login}: account didn't create, because password must contain more than zero symbols lol ";
         }
         throw new NotImplementedException();
+    }
+
+    private async Task<bool> CheckPasswordQuality(UserMainDTO userDTO, PasswordQuality passwordQuality, bool status) {
+        if (passwordQuality == PasswordQuality.BAD) {
+            var err = $"{userDTO} password is too weak";
+            status = false;
+            logger.LogDebug(err);
+        };
+
+        if (!status || !await UserRepository.AddAsync(userDTO)) {
+            var err = $"{userDTO} wasn't added to database";
+            status = false;
+            logger.LogDebug(err);
+        };
+        return status;
+    }
+
+    private static PasswordQuality ValidatePassword(string password) {
+        var qualityPoints = 0;
+        var rules = new List<bool> {
+            password.Length > 0,
+            password.Any(char.IsUpper),
+            password.Any(char.IsLower),
+            password.Any(char.IsDigit)
+        };
+
+        foreach (var rule in rules) {
+            if (rule) {
+                qualityPoints++;
+            }
+        }
+
+        var res = qualityPoints switch {
+            1 => PasswordQuality.BAD,
+            2 => PasswordQuality.WEAK,
+            3 => PasswordQuality.GOOD,
+            4 => PasswordQuality.STRONG,
+            _ => PasswordQuality.STRONG
+        };
+        return res;
     }
 }
---
+++ Zaza/Web/Stuff/InteractLogic/Auth/PasswordQuality.cs
@@ -0,0 +1,9 @@
+﻿namespace Zaza.Web.Stuff.InteractLogic.Auth;
+
+internal enum PasswordQuality {
+    BAD,
+    WEAK,
+    GOOD,
+    STRONG
+}
+
