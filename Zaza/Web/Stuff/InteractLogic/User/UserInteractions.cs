887475b6d9f8ec5dd9d030c62b6a1da4b4e51a11
w1thluv <dimahudric@gmail.com>
2024-03-25 23:56:23 +0300

    Pull user method add

---
+++ Zaza/Web/Stuff/InteractLogic/User/UserInteractions.cs
@@ -0,0 +1,20 @@
+﻿using Zaza.Web.DataBase.Entities;
+using Zaza.Web.DataBase.Repository;
+
+namespace Zaza.Web.Stuff.InteractLogic.User;
+
+internal class UserInteractions(ILogger<UserInteractions> logger, RepositoryContainer repositories) : InteractAbstract(logger, repositories) {
+    public async Task<InteractResult<UserEntity?>> PullUser(HttpContext context) => await PullUser(context.GetId());
+    public async Task<InteractResult<UserEntity?>> PullUser(Guid id) {
+        const InteractEvent INTERACT_EVENT = InteractEvent.RECEIVING;
+        var user = await UserRepository.FindByFilterAsync(FindFilter.ID, id);
+        var res = new InteractResult<UserEntity?>(Success: true, Event: INTERACT_EVENT, Data: user);
+        if (user == null) {
+            var err = $"User {id} wasn't found in db.";
+            logger.LogDebug($"{nameof(PullUser)}: " + err);
+            res = new InteractResult<UserEntity?>(Success: false, Event: INTERACT_EVENT, Data: user, Error: err);
+        }
+
+        return res;
+    }
+}
